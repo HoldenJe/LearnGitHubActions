d14b82797d0147dab637cbf31063d572608e19f8
HoldenJe <71851334+HoldenJe@users.noreply.github.com>
2023-06-05 13:36:16 -0400

    Update lunchtime.r

--- R/lunchtime.r
+++ R/lunchtime.r
@@ -1,14 +1,12 @@
 #' Lunch Time
 #' @description adding a description to test GH-Action
-#' @return string "It's lunch time". That's all it does. Adding some additional text to trigger GH-actions.
+#' @return string "It's lunch time". That's all it does. Adding some additional text to trigger GH-actions. Some extra changes.
 #' @export
 #'
 #' @examples
 #' lunchtime()
 #'
 lunchtime <- function() {
-  # adding a poorly formatted comment
-
   print("It's lunch time")
-  # exit function
+    # exit function
 }
