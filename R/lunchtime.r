205ff583b6c138d2fdaa18fe050eead338863488
HoldenJe <71851334+HoldenJe@users.noreply.github.com>
2023-06-05 11:39:02 -0400

    Create lunchtime.r
    creates a new function to see if stylr and roxygen run

---
+++ R/lunchtime.r
@@ -0,0 +1,14 @@
+#' Lunch Time
+#' @description adding a description to test GH-Action
+#' @return string "It's lunch time". That's all it does.
+#' @export
+#'
+#' @examples
+#' lunchtime()
+#'
+lunchtime <- function() {
+  # adding a poorly formatted comment
+
+  print("It's lunch time")
+  #exit function
+}
